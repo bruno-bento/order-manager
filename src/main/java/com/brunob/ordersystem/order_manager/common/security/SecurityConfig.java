7527839182178923434234793c5bade3e4377a9f
Bruno Bento <brunobentoreinoso14@gmail.com>
2024-12-23 16:45:24 -0300

    core: add security config for disable auth in early development

---
+++ src/main/java/com/brunob/ordersystem/order_manager/common/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.brunob.ordersystem.order_manager.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/**"));
+    }
+}
